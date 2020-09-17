d8c5c877d3759320031b828a8de3d2aa6af11667
vosse <jovicevicdev@gmail.com>
2020-09-18 01:20:54 +0200

    Allow cors

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.use(express.json())
 
 require('dotenv').config()
 
+// Allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+    return res.status(200).json({
+    })
+  }
+  next()
+})
+
 
 const port = process.env.PORT || 5000
 
